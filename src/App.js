d7b0601376ffcfae57fbac0d7e0a492557143495
Lin68 <hu.lha86@gmail.com>
2019-03-08 09:57:13 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import "antd/dist/antd.css";
+import Login from "./components/Login";
+import axios from "axios";
+import StockViewer from "./components/StockViewer";
+import StockViewerTopUp from "./components/StockViewerTopUp";
 
 class App extends Component {
+  //data currently stored in the application
+  state = {
+    username: "",
+    balance: 0,
+    quotes: [],
+    error: ""
+  };
+
+  //given the inserted username a HTTP GET request is made to the db and based on the response
+  //the user is logged in or error is displayed.
+  //state is updated with the user data (response)
+  handleLogin = e => {
+    e.preventDefault();
+    const username = e.target.querySelector('input[name="username"]').value;
+    axios
+      .get(`http://localhost:8302/users/${username}`)
+      .then(response => {
+        const { username, balance, quotes } = response.data;
+        this.setState({ username, balance, quotes });
+      })
+      .catch(error => {
+        this.setState({ error: error.message });
+      });
+  };
+
+  //when the user wants to top up, HTTP PATCH request is sent to the server
+  handleBalanceSubmit = (amount, cb) => {
+    const newBalance = Number(amount) + Number(this.state.balance);
+    const data = {
+      username: this.state.username,
+      balance: newBalance
+    };
+
+    axios
+      .patch(`http://localhost:8302/users/${this.state.username}`, data)
+      .then(response => {
+        this.setState({ balance: newBalance, error: "" });
+      })
+      .then(() => {
+        cb();
+      })
+      .catch(error => {
+        this.setState({ error: error.message });
+      });
+  };
+
+  //when user buys new stock update the balance and the quotes
+  handleQuote = quote => {
+    this.setState({
+      ...this.state,
+      balance: Number(this.state.balance) - Number(quote.acquisitionPrice),
+      quotes: [...this.state.quotes, quote]
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          {this.state.username === "" ? (
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <Login
+                  {...props}
+                  handleLogin={this.handleLogin}
+                  error={this.state.error}
+                />
+              )}
+            />
+          ) : (
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <StockViewer
+                  {...props}
+                  username={this.state.username}
+                  balance={this.state.balance}
+                  quotes={this.state.quotes}
+                  handleQuote={this.handleQuote}
+                />
+              )}
+            />
+          )}
+          <Route
+            path="/top-up"
+            render={props => (
+              <StockViewerTopUp
+                {...props}
+                handleBalanceSubmit={this.handleBalanceSubmit}
+                username={this.state.username}
+              />
+            )}
+          />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/RegisterForm.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import { Form, Icon, Input, Button } from "antd";
+
+class RegisterForm extends Component {}
+
+export default RegisterForm;
